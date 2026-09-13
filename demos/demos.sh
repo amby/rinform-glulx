1e86c1d293c6858275a80ec13f35b62833211c62
Maksim Ilinykh <m.ilinykh@fait.gl>
2026-09-13 16:12:48 +0300

    Fix conflicting words, which Inform 6.44 treats as an error (it is actually an error, but it was ignored by older Inform versions)

--- demos/demos.sh
+++ demos/demos.sh
@@ -1,7 +1,7 @@
-inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$MAX_UNICODE_CHARS=128 AdventR.inf AdventR.ulx
-inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 Heidi.inf Heidi.ulx
-inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 AliceR.inf AliceR.ulx
-inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 CloakR.inf CloakR.ulx
-inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 RuinsR.inf RuinsR.ulx
-inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 WTellR.inf WTellR.ulx
-inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$MAX_UNICODE_CHARS=128 HintsDemo.inf HintsDemo.ulx
+inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$DICT_WORD_SIZE=12 \$MAX_UNICODE_CHARS=128 AdventR.inf AdventR.ulx
+inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$DICT_WORD_SIZE=12 Heidi.inf Heidi.ulx
+inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$DICT_WORD_SIZE=12 AliceR.inf AliceR.ulx
+inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$DICT_WORD_SIZE=12 CloakR.inf CloakR.ulx
+inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$DICT_WORD_SIZE=12 RuinsR.inf RuinsR.ulx
+inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$DICT_WORD_SIZE=12 WTellR.inf WTellR.ulx
+inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$DICT_WORD_SIZE=12 \$MAX_UNICODE_CHARS=128 HintsDemo.inf HintsDemo.ulx
